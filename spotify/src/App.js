4200fcd1f38294e25da4cff58e9fcdd08e95379a
Gustavo Frehse <frehsegustavo@gmail.com>
2025-02-03 02:03:16 +0000

    feat: inicio e projeto base de react

---
+++ spotify/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+// Componentes
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
---
+++ spotify/src/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+
+import smallLeft from "../icons/small-left.png";
+import smallRight from "../icons/small-right.png";
+import searchIcon from "../icons/search.png";
+
+const Header = () => {
+    return (
+        <nav className="header__navigation">
+            <div className="navigation">
+                <button className="arrow-left">
+                    <img src={smallLeft} alt="Left arrow" />
+                </button>
+                <button className="arrow-right">
+                    <img src={smallRight} alt="Right arrow" />
+                </button>
+            </div>
+
+            <div className="header__search">
+                <img src={searchIcon} alt="" />
+                <input id="search-input" maxlength="800" placeholder="What do you want to play?" type="text" />
+            </div>
+
+            <div className="header__login">
+                <button className="subscribe">Sign up</button>
+                <button className="login">Login</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
